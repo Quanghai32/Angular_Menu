d2c2c1b62c9d8d8db54567c98a2befa73fb3c113
Tran Duc Hai <quanghai.dt4@gmail.com>
2017-11-16 18:11:59 +0700

    Menu in angular ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
-declare var $ :any;
+import { Component,OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular 4 with jquery';
-  toggleTitle(){
-    $('.title').slideToggle(); //
+  toggleTitle() {
+    $('.title').slideToggle();
+  }
+  constructor() {
+
+  }
+ 
+  ngOnInit() {
+    this.handle()
   }
 
-//   $('.menu>ul>li').click(function () {
-//     if ($(this).hasClass('active')) {
-//         $(this).children('.submenu').slideUp();
-//         $(this).removeClass('active');
-//     } else {
-//         $('.submenu').slideUp();
-//         $('.menu>ul>li').removeClass('active');
-//         $(this).children('.submenu').slideDown();
-//         $(this).addClass('active');
-//     }
-// });
+  handle() {
+    $('.menu>ul>li').click(function () {
+      if ($(this).hasClass('active')) {
+        $(this).children('.submenu').slideUp();
+        $(this).removeClass('active');
+      } else {
+        $('.submenu').slideUp();
+        $('.menu>ul>li').removeClass('active');
+        $(this).children('.submenu').slideDown();
+        $(this).addClass('active');
+      }
+    });
+  }
 }
